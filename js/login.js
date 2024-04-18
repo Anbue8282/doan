60583e71a5cac8f6c55e20b2e827419cb08557ac
Anbue8282 <122885921+Anbue8282@users.noreply.github.com>
2024-04-18 17:51:41 +0700

    doan

---
+++ js/login.js
@@ -0,0 +1,19 @@
+let item ={
+    image:"",
+    name: "",
+    detail: "",
+    price: "",
+};
+
+function validate_form(){
+
+}
+
+function add_data_in_obj() {
+
+}
+
+function set_localStoreage() {
+
+}
+
